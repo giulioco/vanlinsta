44b8ef2586031ad128febc68d914d4b263e49506
Giulio Colleluori <gcolleluori@splunk.com>
2019-12-19 09:54:22 -0800

    updates to set_cron script

--- set_cron.sh
+++ set_cron.sh
@@ -5,7 +5,7 @@ crontab -l > mycron
 #echo new cron into cron file
 if grep -qF "bot.py" mycron;then
    echo "Entry already exists for ./bot.py. Deleting existing entry..."
-   sed '/bot.py/d' ./mycron > mycron_new
+   sed '/bot.py/d' ./mycron > ./mycron_new
 else
    echo "Entry not found for bot.py"
 fi
@@ -13,7 +13,11 @@ echo "Adding entry..."
 # Sets crontab at minute 0 past every 2nd hour from 8 through 23
 # Sleeps between 0 and 119 minutes
 # Calls bot
-echo "0 8-23/2 * * * echo 'Executing script.' >> botcron.log ; sleep \${RANDOM:0:2%120}m ; cd ~ && /usr/bin/python3 ~/vanlinsta/bot.py >> ~/vanlinsta/bot.log 2>&1" >> mycron_new
+echo "0 8-23/2 * * * sleep \${RANDOM:0:2%120}m && cd ~ && /usr/bin/python3 ~/vanlinsta/bot.py >> /root/vanlinsta/bot.log 2>&1" >> ./mycron_new
 #install new cron file
+echo "Setting the crontab to the following"
+cat mycron_new
 crontab mycron_new
+echo "Crontab set to the following"
+crontab -l
 rm mycron mycron_new
