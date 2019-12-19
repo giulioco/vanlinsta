8d80e0ecf080a35416c5b6ed28cec895f5164664
Giulio Colleluori <gcolleluori@splunk.com>
2019-12-19 07:03:13 -0800

    updated crontab

--- set_cron.sh
+++ set_cron.sh
@@ -13,7 +13,7 @@ echo "Adding entry..."
 # Sets crontab at minute 0 past every 2nd hour from 8 through 23
 # Sleeps between 0 and 119 minutes
 # Calls bot
-echo "0 8-23/2 * * * bash sleep \${RANDOM:0:2%120}m ; cd ~ && /usr/bin/python3 ~/vanlinsta/bot.py >> ~/vanlinsta/bot.log 2>&1" >> mycron
+echo "0 8-23/2 * * * echo 'Executing script.' >> botcron.log ; sleep \${RANDOM:0:2%120}m ; cd ~ && /usr/bin/python3 ~/vanlinsta/bot.py >> ~/vanlinsta/bot.log 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
