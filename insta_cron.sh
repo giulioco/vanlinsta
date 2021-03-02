8ceb599bb8e2ac7cc852f174eb92f1505c487fe5
Giulio Colleluori <thisisgiulio@gmail.com>
2021-03-02 14:20:11 -0800

    Updated insta_cron to delete cookies at the beginning of each session

--- insta_cron.sh
+++ insta_cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SLEEP_TIME=${RANDOM:0:2%120}
 SLEEP_TIME="${SLEEP_TIME}m"
+rm ~/InstaPy/logs/vanlyapp/vanlyapp_cookie.pkl
 sleep $SLEEP_TIME
 /usr/bin/python3 ~/vanlinsta/bot.py >> ~/vanlinsta/bot.log 2>&1
